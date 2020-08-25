5dc31a4a8d5b52b98440f2ab3abd84df62562425
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-08-25 15:27:54 -0700

    Adds a title to Layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,12 @@
 import style from "../styles/Layout.module.css";
 
 const Layout: React.FC = ({ children }) => {
-  return <div className={style.container}>{children}</div>;
+  return (
+    <div className={style.container}>
+      <h1 className={style.title}>The Curator</h1>
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
