269fa03b1aa4c8ad866c68c8c594da3799d00235
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-08-28 10:48:34 -0700

    Resolver.py returns real data:
    - Adds Word2Vec model.
    - Resolver accepts a name arg.

--- python/resolver.py
+++ python/resolver.py
@@ -1,16 +1,22 @@
+import argparse
 import json
+from gensim.models.word2vec import Word2Vec 
 
-def main():
+def main(name):
+  model = Word2Vec.load("./word2vec.pickle")
+  similars = model.wv.most_similar(name)
+
+  tokens = [token for (token, score) in similars]
+
   result = {
-    "name": "Helen",
-    "artists": [
-      'foo1',
-      'foo2',
-      'foo3'
-    ]
+    "name": name,
+    "artists": tokens
   }
   return json.dumps(result)
 
 
 if __name__ == '__main__':
-  print(main())
+  parser = argparse.ArgumentParser(description="Get similar artists from the word2vec model.")
+  parser.add_argument("name", nargs=1)
+  name = parser.parse_args().name[0]
+  print(main(name))
