7f09818ceb639074cb59decbc5294e6265ecf7d4
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-08-28 15:23:17 -0700

    Home displays autocomplete and 10 cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,58 @@
+import { useState } from "react";
+import fetch from "isomorphic-unfetch";
 import Head from "next/head";
+import Layout from "../components/layout";
+import AutoComplete from "../components/autocomplete";
+import style from "../styles/Home.module.css";
+import names from "../lib/names.json";
 
-type Props = {
-  data: string[];
-};
+const Home: React.FC = () => {
+  const [exhibition, setExhibition] = useState<string[]>(Array());
 
-const Home: React.FC<Props> = () => {
-  return (<div>Foo</div>);
+  const onSubmit = async (value: string | null) => {
+    const response = await fetch("http://localhost:3000/api/post", {
+      method: "post",
+      body: JSON.stringify({ name: value }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await response.json();
+    setExhibition(data.artists);
+  };
+
+  return (
+    <Layout>
+      <Head>
+        <title>The Curator</title>
+      </Head>
+      <div className={style.main}>
+        <AutoComplete
+          className={style.autocomplete}
+          onSubmitCallback={(value) => onSubmit(value)}
+          allSuggestions={names}
+        />
+        <div className={style.description}>
+          Enter an artist or select one by{" "}
+          <a
+            href="#"
+            onClick={(e) => {
+              alert("Selecting random artist");
+            }}
+          >
+            random
+          </a>
+        </div>
+        <section>
+          {exhibition.map((artist) => {
+            return (
+              <div data-testid="test-card" key={artist.replace(/\s/g, "")}>
+                {artist}
+              </div>
+            );
+          })}
+        </section>
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,8 +1,21 @@
 import Home from "../../pages/index";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
-describe("Home", () => {
-  it("renders without crashing", () => {
-    render(<Home data={['a', 'b']} />);
+describe("Home without form input", () => {
+  it("includes an autocomplete", () => {
+    const { container, getByTestId } = render(<Home />);
+    const suggestions = getByTestId("autocomplete-artist");
+    expect(suggestions).toBeInTheDocument();
+  });
+});
+
+describe("Home with form input", () => {
+  it("renders ten divs with data-testid=test-card", async () => {
+    const { container, getByRole, findAllByTestId } = render(<Home />);
+    const field = getByRole("textbox");
+    fireEvent.change(field, { target: { value: "Lee" } });
+    fireEvent.keyDown(field, { key: "Enter", code: "Enter" });
+    const cards = await findAllByTestId("test-card");
+    expect(cards).toHaveLength(10);
   });
 });
