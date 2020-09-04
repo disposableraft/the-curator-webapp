01a86e0e69abba1fdcf0c5a351527f5a00602375
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-09-04 07:18:59 -0700

    Throw err when input not in names:
    - Stripping user input of nonletter chars caused errors with some names.
      A better way is to just check that input is in the list.
      No sanitizing necessary.

--- pages/api/post.ts
+++ pages/api/post.ts
@@ -1,20 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import util from "util";
 import { exec } from "child_process";
+import names from "../../lib/names.json";
 
 const execPromise = util.promisify(exec);
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const input = req.body.name;
-  if (input === undefined) {
-    res.json({ error: "Name is undefined" });
-    return;
-  }
-  // Replace all non-letter-or-number chars with space, and trim.
-  const name = input.replace(/\W/g, " ").trim();
   try {
+    if (names.indexOf(input) < 0) {
+      throw new Error(`${input} not in names`);
+    }
     const { stdout, stderr } = await execPromise(
-      `python python/resolver.py "${name}"`
+      `python python/resolver.py "${input}"`
     );
     if (stderr) {
       res.json({ error: stderr });
