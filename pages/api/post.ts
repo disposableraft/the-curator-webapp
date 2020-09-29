effcdda809770c4a5f04305114ee6dc2a5c172d1
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-09-29 11:01:53 -0700

    Factor out python:
    - Each set in the model is static. Therefore, there's no need to host
      the python dependencies.

--- pages/api/post.ts
+++ pages/api/post.ts
@@ -1,24 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import util from "util";
-import { exec } from "child_process";
-import names from "../../lib/names.json";
 
-const execPromise = util.promisify(exec);
+// Get around TS `moduleResolution`
+const top12Map = require("../../lib/top_12_map.json");
+let model: Map<string, string[]>;
+model = new Map(Object.entries(top12Map));
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const input = req.body.name;
+  const input: string = req.body.name;
   try {
-    if (names.indexOf(input) < 0) {
-      throw new Error(`${input} not in names`);
+    if (!model.has(input)) {
+      throw new Error(`"${input}" not in model`);
     }
-    const { stdout, stderr } = await execPromise(
-      `python python/resolver.py "${input}"`
-    );
-    if (stderr) {
-      res.json({ error: stderr });
-    } else {
-      res.json(stdout);
-    }
+    const result = model.get(input);
+    res.json({ name: input, artists: result });
   } catch (error) {
     res.json({ error: error });
   }
