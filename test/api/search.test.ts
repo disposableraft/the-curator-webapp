f985d20369c599209496c5f3aa9d476a723efd6b
Lance W <50839441+disposableraft@users.noreply.github.com>
2020-10-10 09:07:20 -0700

    Deletes api/search

--- test/api/search.test.ts
+++
@@ -1,9 +0,0 @@
-import fetch from "isomorphic-unfetch";
-
-describe("api/search", () => {
-  it("responds with 200", async () => {
-    const reqUrl = new URL("http://localhost:3000/api/search");
-    const response = await fetch(reqUrl.href);
-    expect(response.status).toBe(200);
-  });
-});
